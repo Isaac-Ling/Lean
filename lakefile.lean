14c1851f72ae3bfe1a88ad9d2a5d80fed0d5a233
BluishRed <iling3434@gmail.com>
2023-11-03 15:00:27 +0000

    test

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-package «my_project» {
+package «lean» {
   -- add any package configuration options here
 }
 
@@ -9,6 +9,6 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «MyProject» {
+lean_lib «Lean» {
   -- add any library configuration options here
 }
