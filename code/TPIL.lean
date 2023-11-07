cac4f18b562c29234f2efdbb7bbfc238d3feff58
BluishRed <iling3434@gmail.com>
2023-11-07 17:34:48 +0000

    Added section label

--- code/TPIL.lean
+++ code/TPIL.lean
@@ -1,29 +1,31 @@
-variable (p q r : Prop)
+section chapter_3_exercises
+  variable (p q r : Prop)
 
--- commutativity of ∧ and ∨
-example : p ∧ q ↔ q ∧ p :=
-  Iff.intro
-    (λ (h : p ∧ q) => And.intro h.right h.left)
-    (λ (h : q ∧ p) => And.intro h.right h.left)
-example : p ∨ q ↔ q ∨ p := sorry
+  -- commutativity of ∧ and ∨
+  example : p ∧ q ↔ q ∧ p :=
+    Iff.intro
+      (λ (h : p ∧ q) => And.intro h.right h.left)
+      (λ (h : q ∧ p) => And.intro h.right h.left)
+  example : p ∨ q ↔ q ∨ p := sorry
 
--- associativity of ∧ and ∨
-example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
-example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+  -- associativity of ∧ and ∨
+  example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+  example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
 
--- distributivity
-example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
-example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+  -- distributivity
+  example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+  example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
 
--- other properties
-example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
-example : p ∨ False ↔ p := sorry
-example : p ∧ False ↔ False := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+  -- other properties
+  example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+  example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+  example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+  example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+  example : ¬(p ∧ ¬p) := sorry
+  example : p ∧ ¬q → ¬(p → q) := sorry
+  example : ¬p → (p → q) := sorry
+  example : (¬p ∨ q) → (p → q) := sorry
+  example : p ∨ False ↔ p := sorry
+  example : p ∧ False ↔ False := sorry
+  example : (p → q) → (¬q → ¬p) := sorry
+end chapter_3_exercises
